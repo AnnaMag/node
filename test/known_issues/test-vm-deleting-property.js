b2f0b01faa9cb1796f992f4efc38e9e5b22680b6
AnnaMag <AnnaMag@users.noreply.github.com>
2016-12-16 20:16:29 +0100

    test-vm-deleting-property

--- test/known_issues/test-vm-deleting-property.js
+++ test/known_issues/test-vm-deleting-property.js
@@ -1,15 +1,15 @@
 'use strict';
-// Refs:https://github.com/nodejs/node/issues/6287
+// Refs: https://github.com/nodejs/node/issues/6287
 
 require('../common');
 const assert = require('assert');
 const vm = require('vm');
 
-var context = vm.createContext();
+const context = vm.createContext();
 const res = vm.runInContext(`
   this.x = 'prop';
   delete this.x;
-  Object.getOwnPropertyDescriptor(this, "x");
+  Object.getOwnPropertyDescriptor(this, 'x');
 `, context);
 
-assert.equal(res.value, undefined); 
+assert.strictEqual(res.value, undefined);
