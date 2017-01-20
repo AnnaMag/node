424bacfdabb17fa3dd96434bd8e6e83e3caaeafc
AnnaMag <AnnaMag@users.noreply.github.com>
2017-01-20 22:10:57 +0000

    add helper string print

--- src/helpers.h
+++ src/helpers.h
@@ -5,6 +5,8 @@
 using v8::Local;
 using v8::String;
 using v8::Array;
+using v8::Isolate;
 
 void PrintLocalString(v8::Local<v8::String> key);
 void PrintLocalArray(v8::Local<v8::Array> arr);
+Local<String> StdStringToLocalString(std::string sText, v8::Isolate* isolate);
