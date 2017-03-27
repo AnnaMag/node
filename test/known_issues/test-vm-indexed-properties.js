ef297f443b4dd2cd903c5b535d2673bf70af5b38
AnnaMag <AnnaMag@users.noreply.github.com>
2017-03-27 20:08:08 +0100

    move tests

--- test/known_issues/test-vm-indexed-properties.js
+++ test/known_issues/test-vm-indexed-properties.js
@@ -4,14 +4,14 @@ require('../common');
 const assert = require('assert');
 const vm = require('vm');
 
-const code =
-    '  Object.defineProperty(this, 99 , {\n' +
-    '  value: 20,' +
-    '  enumerable: true' +
-    '});';
+const code = `Object.defineProperty(this, 99, {
+      value: 20,
+      enumerable: true
+ });`;
 
-const sandbox = {console}
-let ctx = vm.createContext(sandbox);
-vm.runInContext(code, ctx)
 
-assert.strictEqual(sandbox[99], 20); //  sandbox[99] is undefined
+const sandbox = {};
+const ctx = vm.createContext(sandbox);
+vm.runInContext(code, ctx);
+
+assert.strictEqual(sandbox[99], 20);
